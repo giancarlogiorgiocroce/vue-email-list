82a1b1cf33ffb678b322665f9768ea430aaeb824
giancarlogiorgiocroce <giancarlo.giorgio.croce@gmail.com>
2022-05-11 14:46:20 +0200

    Stampa delle email completata

--- js/script.js
+++ js/script.js
@@ -4,3 +4,28 @@ generare 10 indirizzi email e stamparli in pagina all’interno di una lista.
 
 2. Far comparire gli indirizzi email solamente quando sono stati tutti generati.
 */
+
+const app = new Vue({
+    el: "#app",
+
+    data: {
+        emails: [],
+        totalNumberOfEmails: 10,
+    },
+
+    methods: {
+        getEmail(){
+            for(let i = 0; i < this.totalNumberOfEmails; i++){
+                axios.get("https://flynn.boolean.careers/exercises/api/random/mail")
+                .then((res)=>{
+                    this.emails.push(res.data.response);
+                    console.log(this.emails);
+                })
+            }
+        }
+    },
+
+    mounted(){
+        this.getEmail();
+    }
+})
